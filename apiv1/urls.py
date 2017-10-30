280a745cda38b6646a87d9a81a30d06556a5bd43
Amresh Kumar <amresh8.kumar@gmail.com>
2017-10-30 23:58:11 +0530

    add api to place order from app

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from rest_framework.authtoken import views
-from .views import DistributerViewSet, ProductViewSet
+from .views import DistributerViewSet, ProductViewSet, place_order
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -11,5 +11,6 @@ router.register(r'product', ProductViewSet, base_name='Product')
 # Wire up our API using automatic URL routing.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^auth/', views.obtain_auth_token)
+    url(r'^auth/', views.obtain_auth_token),
+    url(r'place_order/', place_order)
 ]
--- apiv1/views.py
+++ apiv1/views.py
@@ -2,8 +2,12 @@
 
 from partner import models
 from . import data_serializers
+from datetime import datetime
 
 from rest_framework import viewsets
+from rest_framework import parsers, renderers
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # ViewSets define the view behavior.
@@ -25,3 +29,51 @@ class ProductViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         distributor_id = self.request.query_params['id']
         return models.Product.objects.filter(distributer__user_id__exact=distributor_id)
+
+
+class PlaceOrder(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+
+    def post(self, request, *args, **kwargs):
+        # parse data from POST request
+        retailer_id = request.data['retailer_id']
+        distributor_id = request.data['distributor_id']
+        products = request.data['products']
+        quantity = request.data['quantity']
+
+        # fetch retailer and distributor accounts
+        retailer = models.Retailer.objects.filter(user_id=retailer_id).first()
+        distributor = models.Distributer.objects.filter(user_id=distributor_id).first()
+
+        if retailer is None or distributor is None:
+            return Response(status=400, exception=True,
+                            data={'error': 'requested retailer or distributor does not exist.'})
+
+        # fetch all products to validate ids
+        for item_id in products:
+            product = models.Product.objects.filter(id=item_id).first()
+            if product is None:
+                return Response(status=400, exception=True,
+                                data={'error': 'requested product with id ' + str(item_id) + ' does not exist.'})
+
+        # create order object with retailer and distributor
+        order = models.Order.objects.create(retailer=retailer,
+                                            distributer=distributor,
+                                            order_date=datetime.now(),
+                                            order_status='REQUESTED_APP')
+
+        # create order items for this order
+        for item_id, qty in zip(products, quantity):
+            product = models.Product.objects.filter(id=item_id).first()
+
+            models.OrderItem.objects.create(order=order,
+                                            product=product,
+                                            item_quantity=qty)
+
+        return Response({'status': '200', 'data': request.data})
+
+
+place_order = PlaceOrder.as_view()
