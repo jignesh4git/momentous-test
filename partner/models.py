45a3e3f0546b11ccaec4dfe2f5a414b62215d03d
Amresh Kumar <amresh8.kumar@gmail.com>
2017-12-09 10:36:04 +0530

    refactor models

--- partner/models.py
+++ partner/models.py
@@ -1,60 +1,44 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
-from django.contrib import admin
 
-
-# Create your models here.
 
 class Partner(models.Model):
-    name = models.CharField(max_length=250)
+    PARTNER_TYPE = (
+        ('manufacturer', 'Manufacturer'),
+        ('distributor', 'Distributor'),
+        ('retailer', 'Retailer'),
+        ('employee', 'Employee'),
+    )
 
+    PERMISSIONS = (
+        ('sell_product', 'Sell Product'),
+        ('sell_add_product', 'Add & Sell Product'),
+        ('sell_add_product_partner', 'Add & Sell Product, Add Partner'),
+    )
 
-class Manufacturer(models.Model):
     user = models.OneToOneField(User)
     mobile_number = PhoneNumberField()
+    alternate_number = PhoneNumberField()
     company_name = models.CharField(max_length=255, blank=False)
-    company_address = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
     pin_code = models.CharField(max_length=255)
     GSTIN = models.CharField(max_length=255, blank=True)
     PAN = models.CharField(max_length=255, blank=True)
+    ADHAAR = models.CharField(max_length=255, blank=True)
+
+    type = models.CharField(max_length=255, choices=PARTNER_TYPE, blank=False)
+    buys_from = models.ManyToManyField('self')
+    sells_to = models.ManyToManyField('self')
+
+    permissions = models.CharField(max_length=255, choices=PERMISSIONS, blank=True)
 
     def __str__(self):
         return "{}".format(self.company_name)
 
 
-class Distributor(models.Model):
-    user = models.OneToOneField(User)
-    mobile_number = PhoneNumberField()
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=True)
-    company_name = models.CharField(max_length=255, blank=False)
-    company_address = models.CharField(max_length=255)
-    pin_code = models.CharField(max_length=255)
-    GSTIN = models.CharField(max_length=255, blank=True)
-    PAN = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return "{}".format(self.company_name)
-
-
-class Retailer(models.Model):
-    user = models.OneToOneField(User)
-    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
-    store_name = models.CharField(max_length=255, blank=False)
-    store_number = PhoneNumberField()
-    mobile_number = PhoneNumberField(blank=True)
-    store_address = models.CharField(max_length=255)
-    pin_code = models.CharField(max_length=255)
-    GSTIN = models.CharField(max_length=255, blank=True)
-    PAN = models.CharField(max_length=255, blank=True)
-
-    def __str__(self):
-        return "{}".format(self.store_name, self.store_number)
-
-
-class Product(models.Model):
-    # distributor = models.ForeignKey(Distributor)
-    manufacturer = models.ForeignKey(Manufacturer, null=True)
+class BaseProduct(models.Model):
+    manufacturer = models.ForeignKey(Partner, null=True)
     code = models.CharField(max_length=255, blank=True)
     name = models.CharField(max_length=255, blank=False)
     packing = models.CharField(max_length=255, blank=False)
@@ -68,63 +52,47 @@ class Product(models.Model):
     s_gst = models.IntegerField(choices=GST_CHOICES, default=0)
     c_gst = models.IntegerField(choices=GST_CHOICES, default=0)
     category = models.CharField(max_length=255, blank=True)
-    HSN = models.CharField(max_length=255, blank=True)
-    active = models.BooleanField()
+
     def __str__(self):
         return "{}".format(self.name, self.manufacturer, self.packing)
 
-class DistributorProduct(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.CharField(max_length=255, blank=True)
-    offer_id = models.CharField(max_length=255, blank=True)
-    final_price = models.CharField(max_length=255, blank=True)
-    active = models.BooleanField()
-    def __str__(self):
-        return "{}".format(self.product,self.distributor,self.manufacturer)
 
-class RetailerProduct(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
-    retailer = models.ForeignKey(Retailer, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    price = models.CharField(max_length=255, blank=True)
-    offer_id = models.CharField(max_length=255, blank=True)
-    final_price = models.CharField(max_length=255, blank=True)
-    active = models.BooleanField()
-    def __str__(self):
-        return "{}".format(self.retailer,self.distributor)
+class Product(models.Model):
+    base = models.ForeignKey(BaseProduct)
+    selling_price = models.FloatField()
+    is_active = models.BooleanField()
+
 
 class Order(models.Model):
-    retailer = models.ForeignKey(Retailer, related_name='retailer', null=True)
-    distributor = models.ForeignKey(Distributor, related_name='distributor')
-    manufacturer = models.ForeignKey(Manufacturer, related_name='manufacturer', null=True)
-    order_date = models.DateField(auto_created=True)
+    order_from = models.ForeignKey(Partner)
+    order_to = models.ForeignKey(Partner)
+
     order_status = models.CharField(max_length=255, blank=False)
-    # requested_delivery_time = models.DateField(blank=True)
+    order_date = models.DateField(auto_created=True)
+    delivery_date = models.DateField(blank=True)
+
+    requested_delivery_time = models.DateField(blank=True)
     item_total = models.CharField(max_length=255, blank=True)
-    s_gst = models.CharField(max_length=255, blank=True)
-    c_gst = models.CharField(max_length=255, blank=True)
+    s_gst_total = models.CharField(max_length=255, blank=True)
+    c_gst_total = models.CharField(max_length=255, blank=True)
     other_charge_description = models.CharField(max_length=255, blank=True)
     other_charge = models.CharField(max_length=255, blank=True)
     bill_total = models.CharField(max_length=255, blank=True)
-    invoice_id = models.CharField(max_length=255,blank=True)
+    invoice_id = models.CharField(max_length=255, blank=True)
 
-    # delivery_date = models.DateField(blank=True)
-    # Metadata
     class Meta:
         ordering = ["-order_date"]
+
     def make_id(self):
         q = Order.objects.values_list('id', flat=True).order_by('-id')[:1]
         if len(q):
             self.number = str(self.id) if self.id else str(int(q.get()) + 1)
         else:
             self.number = 1
-        return "SEDIST" + str(self.distributor_id) + "RET" + str(self.retailer_id) + "-" + str(self.number)
+        return "SEFRM" + str(self.order_from.user.id) + "TO" + str(self.order_to.user.id) + "-" + str(self.number)
 
     def __str__(self):
-        return "{}".format(self.id, self.retailer)
+        return "{}".format(self.id)
 
     def save(self, *args, **kwargs):
         if not self.invoice_id:
@@ -137,11 +105,12 @@ class OrderItem(models.Model):
     product = models.ForeignKey(Product)
     item_quantity = models.IntegerField()
 
-    # item_price = models.IntegerField(blank=True)
-    # total = models.IntegerField(blank=True)
+    total = models.IntegerField(blank=True)
     # offer_id = models.CharField(max_length=25, blank=True)
-    # s_gst = models.CharField(max_length=255, blank=True)
-    # c_gst = models.CharField(max_length=255, blank=True)
+    # change to offer
+    s_gst = models.CharField(max_length=255, blank=True)
+    c_gst = models.CharField(max_length=255, blank=True)
+
     class Meta:
         verbose_name_plural = "Invoices"
 
@@ -149,24 +118,14 @@ class OrderItem(models.Model):
         return "{}".format(self.order_id, self.id, self.product_id)
 
 
-class ConnectedRetailer(models.Model):
-    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
-    retailer = models.ForeignKey(Retailer, on_delete=models.CASCADE)
+class ConnectedPartner(models.Model):
+    first_partner = models.ForeignKey(Partner)
+    second_partner = models.ForeignKey(Partner)
     credit_limit = models.IntegerField()
     remaining = models.IntegerField()
 
     def __str__(self):
-        return "{}".format(self.distributor, self.retailer)
-
-
-class ConnectedDistributor(models.Model):
-    distributor = models.ForeignKey(Distributor, on_delete=models.CASCADE)
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    credit_limit = models.IntegerField()
-    remaining = models.IntegerField()
-
-    def __str__(self):
-        return "{}".format(self.distributor,self.manufacturer)
+        return "{}".format(self.first_partner, self.second_partner)
 
 
 class UserProfile(models.Model):
