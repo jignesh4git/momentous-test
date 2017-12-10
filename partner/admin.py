d63520420d800f39e75bd3b1317ac5daed80f596
jignesh4git <dba.jigneshpatel@gmail.com>
2017-12-10 22:40:16 +0530

    fix product admin view

--- partner/admin.py
+++ partner/admin.py
@@ -42,12 +42,12 @@ class BaseProductAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('partner','product_partner','base','selling_price','is_active')
 
-    def get_queryset(self, request):
-        partner = models.Partner.objects.filter(user=request.user)
-        if not request.user.is_superuser:
-           # partner = models.Partner.objects.filter(user=request.user)
-           return models.Product.objects.filter(partner=partner)
-        return models.Product.objects.all()
+    # def get_queryset(self, request):
+    #     partner = models.Partner.objects.filter(user=request.user)
+    #     if not request.user.is_superuser:
+    #        # partner = models.Partner.objects.filter(user=request.user)
+    #        return models.Product.objects.filter(partner=partner)
+    #     return models.Product.objects.all()
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         partner = models.Partner.objects.filter(user=request.user)
         if not request.user.is_superuser:
