650b993cb88fadfd01bfc8c1e5245e5a1eb76173
jignesh4git <jignesh@healthyhum.com>
2017-12-10 10:08:13 +0530

    Update admin.py
    Fix partner view

--- partner/admin.py
+++ partner/admin.py
@@ -8,11 +8,12 @@ from django.contrib.auth.models import Group
 class PartnerAdmin(admin.ModelAdmin):
     list_display=('company_name','mobile_number','address','GSTIN','PAN','ADHAAR')
 
-   # def get_queryset(self, request):
-       # if not request.user.is_superuser:
+     def get_queryset(self, request):
+         partner = models.Partner.objects.filter(user=request.user)   
+         if not request.user.is_superuser:
         #    partner = models.Partner.objects.filter(user=request.user)
-        #return models.Partner.objects.filter(id=partner)
-       # return models.Partner.objects.all()
+         return models.Partner.objects.filter(id=partner)
+         return models.Partner.objects.all()
 
 
 class ConnectedPartnerAdmin(admin.ModelAdmin):
