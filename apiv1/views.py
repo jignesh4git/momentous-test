0bc3c38d50e76c960a6aa26b0b1d6928bde62d34
Amresh Kumar <amresh8.kumar@gmail.com>
2017-12-17 21:15:39 +0530

    fix bug

--- apiv1/views.py
+++ apiv1/views.py
@@ -27,7 +27,7 @@ class ManufacturerViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        return models.ConnectedPartner.objects.filter(connected_partner=user, type='manufacturer')
+        return models.ConnectedPartner.objects.filter(type='manufacturer', connected_partner__user=user)
 
     serializer_class = data_serializers.ManufacturerAccountSerializer
 
