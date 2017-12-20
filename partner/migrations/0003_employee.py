445716ba8be223799960a608162244dc213895fd
jignesh4git <jignesh@healthyhum.com>
2017-12-20 11:15:33 +0530

    Fix migration error

--- partner/migrations/0003_employee.py
+++ partner/migrations/0003_employee.py
@@ -14,18 +14,3 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
         ('partner', '0002_auto_20171219_1303'),
     ]
-
-    operations = [
-        migrations.CreateModel(
-            name='Employee',
-            fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=255)),
-                ('last_name', models.CharField(max_length=255)),
-                ('mobile_no', phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128)),
-                ('permissions', models.CharField(blank=True, choices=[('sell_product', 'Sell Product'), ('add_product', 'Add Product'), ('add_partner', 'Add Partner')], max_length=255)),
-                ('partner', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='partner.Partner')),
-                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-    ]
